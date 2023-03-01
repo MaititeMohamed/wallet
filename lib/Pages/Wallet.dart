f49e0bfcbdae2107c4535b84d639dc9aa9f95839
MAITITE MOHAMED <maititemohamed@gmail.com>
2023-03-01 09:34:08 +0100

    create Wallet page

---
+++ lib/Pages/Wallet.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Wallet extends StatefulWidget {
+  @override
+  _WalletState createState() => _WalletState();
+}
+
+class _WalletState extends State<Wallet> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Wallet'),
+    );
+  }
+}
