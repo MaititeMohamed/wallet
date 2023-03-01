2b6161de8b6fc140a7ee87efc5b16752658dc01f
MAITITE MOHAMED <maititemohamed@gmail.com>
2023-03-01 09:34:43 +0100

    use the auther page in the main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,88 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import 'Pages/Home.dart';
+import 'Pages/Profile.dart';
+import 'Pages/Settings.dart';
+import 'Pages/Wallet.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
 
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [    Home(),    Wallet(),    Profile(),   Settings()  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+          primaryColor: Colors.blue,
+          accentColor: Colors.orange
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.account_balance_wallet),
+            onPressed: () { /* do something */ },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+              ),
+              onPressed: () { /* do something */ },
+            ),
+          ],
+          //title: Text('Bottom Navigation Bar Example'),
+        ),
+        body: _children[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: onTabTapped,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: true,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home'
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_balance_wallet),
+                label: 'Wallet'
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile'
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings'
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
 
